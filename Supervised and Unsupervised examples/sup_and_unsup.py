f79ba2326fea8700b765e12d9eca8a341bcd6627
rjczanik <Stainless101!>
2020-09-03 19:35:24 +0200

    Supervised and Unsupervised learning

---
+++ Supervised and Unsupervised examples/sup_and_unsup.py
@@ -0,0 +1,17 @@
+### Examples between supervised and unsupervised machine learning algorithms.
+
+### SUPERVISED LEARNING ###
+## Classification - Binary classification
+# Email spam classification. - Logistic regression
+# Refer to https://towardsdatascience.com/email-spam-detection-1-2-b0e06a5c0472
+
+# Regression - Simple linear regression
+# Refer to the linear regression example showed lin_reg_case_study
+
+
+### UNSUPERVISED LEARNING ###
+## Clustering - K means clustering
+# Refer to https://datatofish.com/k-means-clustering-python/
+
+## Association in hierarchies.
+# Refer to https://stackabuse.com/hierarchical-clustering-with-python-and-scikit-learn/
